217164e615f0aeb3445629c6229b4e1a91fc8dbe
Gonzalo Quiroga <gonzaquirogasassu@gmail.com>
2023-04-21 18:58:49 -0300

    Ejercicios JAVASCRIPT Semana 1

---
+++ JavaScript/Asistencia/Semana1/break.js
@@ -0,0 +1,9 @@
+for (let conteoBreak = 0; conteoBreak <= 10; conteoBreak++) {
+  if (conteoBreak % 2 === 0) {
+    console.log(conteoBreak);
+  }
+  if (conteoBreak === 6) {
+    break;
+  }
+}
+console.log("End Break...");
---
+++ JavaScript/Asistencia/Semana1/doWhile.js
@@ -0,0 +1,6 @@
+let conteoDoWhile = 0;
+do {
+  console.log(conteoDoWhile);
+  conteoDoWhile++;
+} while (conteoDoWhile < 3);
+console.log("End DoWhile...");
