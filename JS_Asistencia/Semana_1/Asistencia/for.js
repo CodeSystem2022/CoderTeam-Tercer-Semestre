39cc60d237403a95ec12bc112a503f2cc67ff37e
Gonzalo Quiroga Sassu <gonzaquirogasassu@gmail.com>
2023-04-23 20:56:08 -0300

    Ejercicios JAVASCRIPT Semana 1

---
+++ JS_Asistencia/Semana_1/Asistencia/for.js
@@ -0,0 +1,4 @@
+for (let conteoFor = 0, c = 0; conteoFor < 3; conteoFor++) {
+  console.log(conteoFor);
+}
+console.log("End For...");
---
+++ JS_Asistencia/Semana_1/Asistencia/while.js
@@ -0,0 +1,6 @@
+let conteoWhile = 0;
+while (conteoWhile < 3) {
+  console.log(conteoWhile);
+  conteoWhile++;
+}
+console.log("End While...");
