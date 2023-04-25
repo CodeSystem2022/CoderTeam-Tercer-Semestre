5cfcdcc35c19862f383244865a6c31122b50c529
Vero Cardenas <97110062+Verocardenas0794@users.noreply.github.com>
2023-04-25 17:27:59 -0300

    Ejercicios PYTHON Semana 2

--- Python_Asistencia/Semana_2/Asistencia/Leer_archivo.py
+++ Python_Asistencia/Semana_2/Asistencia/Leer_archivo.py
@@ -5,7 +5,7 @@ archivo = open('prueba.txt','r', encoding='utf8')#Las letras son 'r'
 #print(archivo.readline())
 #print(archivo.readline())
 
-#vamos a iterar el archivo en cada una de las lineas
+#Vamos a iterar el archivo en cada una de las lineas
 #for linea in archivo:
     #print(linea) iteramos todos los elementos del archivo
 #print(archivo.readlines()[11]): #accedemos al archivo como si fuera una lista
@@ -17,6 +17,7 @@ archivo.close() #cerramos el primer archivo
 archivo2.close() #cerramos el segundo archivo
 
 print('Se ha terminado el proceso de leer y copiar archivos')
+
 
 
 
