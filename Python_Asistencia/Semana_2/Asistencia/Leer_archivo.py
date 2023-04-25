f90f83d837b0e6340948b54846a4349ecfad1f26
Vero Cardenas <97110062+Verocardenas0794@users.noreply.github.com>
2023-04-25 17:26:51 -0300

    Add files via upload
    Ejercicios PYTHON Semana 2

---
+++ Python_Asistencia/Semana_2/Asistencia/Leer_archivo.py
@@ -0,0 +1,32 @@
+archivo = open('prueba.txt','r', encoding='utf8')#Las letras son 'r'
+#print(archivo.read())
+#print(archivo.read(5))
+#print(archivo.read(5))#Continuamos desde la linea anterior
+#print(archivo.readline())
+#print(archivo.readline())
+
+#vamos a iterar el archivo en cada una de las lineas
+#for linea in archivo:
+    #print(linea) iteramos todos los elementos del archivo
+#print(archivo.readlines()[11]): #accedemos al archivo como si fuera una lista
+
+#Anexamos información, copiamos otro
+archivo2 = open('copia.txt', 'w', encoding='UTF8')
+archivo2.write(archivo.read())
+archivo.close() #cerramos el primer archivo
+archivo2.close() #cerramos el segundo archivo
+
+print('Se ha terminado el proceso de leer y copiar archivos')
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Python_Asistencia/Semana_2/Asistencia/manejo_archivos.py
@@ -0,0 +1,35 @@
+#Declaramos una variable
+
+try:
+    archivo = open('prueba.txt', 'w',encoding='utf8' )#La w es de write
+    archivo.write('Programamos con diferentes tipos de archivos, ahora en txt.\n')
+    archivo.write('Los acentos son importantes para las palabras.\n')
+    archivo.write('Como por ejemplo: acción, ejecución y producción.\n')
+    archivo.write('Las letras son: \nr read, \na append, \nw write, \nx crea un archivo')
+    archivo.write('\nt esta es para texto o text, \nb archivos binarios, \nw+ lee y escribe son iguales r+\n')
+    archivo.write('Saludos a todos los alumnos de la tecnicatura\n')
+    archivo.write('Con esto terminamos')
+except Exception as e:
+    print(e)
+finally: #siempre se ejecuta
+    archivo.close()#Para que se cierre el archivo
+#archivo.write('Todo quedó perfecto'): este es un error
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
