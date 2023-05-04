d092f64563f4f7153912a7b810f7f711d93b50dd
Adan Gomez <112594636+2022Adan@users.noreply.github.com>
2023-05-03 23:09:20 -0300

    Add files via upload

---
+++ JAVA_Asistencia/Semana3/Clase1.java
@@ -0,0 +1,16 @@
+
+package paquete1;
+
+public class Clase1 {
+   public String atributoPublic = "Valor atributo public";
+   
+   public Clase1(){
+       System.out.println("Constructor publico");
+   }
+   
+   public void metodoPublico() {
+       System.out.println("Metodo publico");
+   }
+    
+
+}
